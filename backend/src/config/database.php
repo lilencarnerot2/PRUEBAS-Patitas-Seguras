3b0fe3b1962536a31d8ec78b5ffbf37b3d02cb2d
lilencarnerot2 <lilencarnerot2@gmail.com>
2026-08-17 21:18:48 -0300

    agrego config de database local

---
+++ backend/src/config/database.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$dbname = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+
+try {
+    $conexion = new PDO(
+        "pgsql:host=$host;port=$port;dbname=$dbname",
+        $user,
+        $password
+    );
+
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
+}
