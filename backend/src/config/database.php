5f314d042be592934826ab63ea84af85ab813cd7
lilencarnerot2 <lilencarnerot2@gmail.com>
2026-08-18 17:21:34 -0300

    modicacion config database para conexion

--- backend/src/config/database.php
+++ backend/src/config/database.php
@@ -1,20 +1,35 @@
 <?php
 
-$host = getenv('DB_HOST');
-$port = getenv('DB_PORT');
-$dbname = getenv('DB_NAME');
-$user = getenv('DB_USER');
-$password = getenv('DB_PASSWORD');
+function conectarDB()
+{
+    $host = getenv('DB_HOST');
+    $port = getenv('DB_PORT');
+    $dbname = getenv('DB_NAME');
+    $user = getenv('DB_USER');
+    $password = getenv('DB_PASSWORD');
 
-try {
-    $conexion = new PDO(
-        "pgsql:host=$host;port=$port;dbname=$dbname",
-        $user,
-        $password
-    );
+    try {
 
-    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conexion = new PDO(
+            "pgsql:host=$host;port=$port;dbname=$dbname",
+            $user,
+            $password
+        );
 
-} catch (PDOException $e) {
-    die("Error de conexión: " . $e->getMessage());
+        $conexion->setAttribute(
+            PDO::ATTR_ERRMODE,
+            PDO::ERRMODE_EXCEPTION
+        );
+
+        $conexion->setAttribute(
+            PDO::ATTR_DEFAULT_FETCH_MODE,
+            PDO::FETCH_ASSOC
+        );
+
+        return $conexion;
+
+    } catch (PDOException $e) {
+
+        die("Error de conexión a la base de datos: " . $e->getMessage());
+    }
 }
