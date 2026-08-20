f23cd200afe2e6f097f75ba3678e01a4f06471f8
lilencarnerot2 <lilencarnerot2@gmail.com>
2026-08-20 18:37:36 -0300

    modifique mascotas a mascota

--- backend/public/index.php
+++ backend/public/index.php
@@ -17,7 +17,7 @@ header('Content-Type: application/json; charset=utf-8'); # instruccion q avisa q
 $recurso = $_GET['recurso'] ?? '';
 $id = $_GET['id'] ?? null;
 
-if ($recurso === 'mascotas') {
+if ($recurso === 'mascota') {
 
     try {
 
